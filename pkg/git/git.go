0c7a7fd200e457576f3983bf05549c41ea577887
Egor Margineanu <egor.margineanu@kyndryl.com>
2021-12-14 15:24:06 +0100

    Fixed url

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -12,7 +12,7 @@ import (
 // CloneRepositoryShallow will clone repository in a given directory.
 func CloneRepositoryShallow(url string, tag string, destDir string) error {
 	{
-		cmd := exec.Command("git", "clone", "-b", tag, "--depth", "1", fmt.Sprintf("https://%s.git", url), destDir)
+		cmd := exec.Command("git", "clone", "-b", tag, "--depth", "1", fmt.Sprintf("%s.git", url), destDir)
 		err := cmd.Run()
 		if err != nil {
 			return microerror.Maskf(errorpkg.ExecutionError, "Could not `git clone` source repository.\nTried to execute: %s\n%s", cmd.String(), err.Error())
